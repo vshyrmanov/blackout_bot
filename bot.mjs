515b8bd234d9b7e205fb05d370c65c27c2665b05
vshyrmanov <shyrmanovvm@gmail.com>
2023-01-31 10:05:28 +0200

    set my own bot

--- bot.mjs
+++ bot.mjs
@@ -1,7 +1,25 @@
 import TeleBot from "telebot"
+import Ping from "ping";
 
 const bot = new TeleBot(process.env.TG_TOKEN)
+const IP = process.env.IP;
 
-bot.on('text', msg => msg.reply.text(msg.text))
+const test = async () => {
+	let res = await Ping.promise.probe(IP);
+	return res.alive
+}
+
+// bot.on('text', msg => msg.reply.text(msg.text))
+bot.on('message', async (msg) => {
+	const chatId = msg.chat.id;
+	const text = msg.text;
+	if (text === '/start') {
+		await bot.sendMessage(chatId, 'Привіт друже')
+	}
+	if (text === '/black') {
+		const msg = await test()
+		await bot.sendMessage(chatId, `${msg ? "Sun" : "Black"}`)
+	}
+})
 
 export default bot
